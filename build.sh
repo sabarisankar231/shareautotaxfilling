71979155ddb15a46a8dfd1a1dcb6b01179ec60e6
sabarisankar231 <sabarisalmart121@gmail.com>
2025-05-14 15:00:13 +0530

    Add files via upload

---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+pip install flask gunicorn
+pip install --index-url https://phonepe.mycloudrepo.io/public/repositories/phonepe-pg-sdk-python --extra-index-url https://pypi.org/simple phonepe_sdk
